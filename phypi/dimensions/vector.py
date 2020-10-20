dc32d2ffa2c70e0b09405d101d4a1ee023cebe51
manas1316am <ad1316a@gmail.com>
2020-10-20 16:36:35 +0530

    added vector multiplication

--- phypi/dimensions/vector.py
+++ phypi/dimensions/vector.py
@@ -1,5 +1,9 @@
 '''
-    1. Magnitude of the vector
+    1. 2-D Magnitude of the vector => twodmagnitude(a,b)
+    2. 3-D Magnitude OF the vector => threedmagnitude(a,b,c)
+
+    3. 2-D dot product => twoddotproduct(a1, b1, a2, b2)
+    4. 3-D dot product => threeddotproduct(a1, b1, a2, b2, a3, b3)
 
 '''
 
@@ -22,3 +26,19 @@ def threedmagnitude(a: int, b: int, c: int):
         >> print(vector.threedmagnitude(a,b,c))
     '''
     return math.sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+
+def twoddotproduct(a1: int, b1: int, a2: int, b2: int):
+    '''
+        [+] twoddotproduct(a1: int, b1: int, a2: int, b2: int)
+        >> from phypi.dimensions import vector
+        >> print(vector.twoddotproduct(a1, b1, a2, b2))
+    '''
+    return a1*b1 + a2*b2 
+
+def threeddotproduct(a1: int, b1: int, a2: int, b2: int, a3: int, b3: int):
+    '''
+        [+] threeddotproduct(a1: int, b1: int, a2: int, b2: int)
+        >> from phypi.dimensions import vector
+        >> print(vector.threeddotproduct(a1, b1, a2, b2, a3, b3))
+    '''
+    return a1*b1 + a2*b2 + a3*b3
